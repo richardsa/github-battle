f9aa350dde1e6170f581508af0e7c18f5d8422b5
Richard Saldivar <rsaldiva@gmail.com>
2016-08-11 00:40:23 +0000

    finish Rendering UI video

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -15,10 +15,10 @@ function ConfirmBattle (props) {
     : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
-         <UserDetailsWrapper>
+         <UserDetailsWrapper header="Player One" >
              <UserDetails info={props.playersInfo[0]} />
          </UserDetailsWrapper>
-         <UserDetailsWrapper>
+         <UserDetailsWrapper header="Player Two">
             <UserDetails info={props.playersInfo[1]} />
         </UserDetailsWrapper>
         <div className='col-sm-8 col-sm-offset-2'>
--- app/components/UserDetailsWrapper.js
+++ app/components/UserDetailsWrapper.js
@@ -4,7 +4,7 @@ var PropType = React.PropTypes;
 function UserDetailsWrapper (props) {
     return (
          <div className='col-sm-6'>
-            <p className="lead">Player 1</p>
+            <p className="lead">{props.header}</p>
             {props.children}
         </div>
         )
