dfed53b3f14b0bab8ceefc0b093b3045b8213858
Richard Saldivar <rsaldiva@gmail.com>
2016-08-12 00:51:59 +0000

    finish More Container vs Presentational Components video

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -6,43 +6,37 @@ var ConfirmBattleContainer = React.createClass({
   contextTypes: {
     router: React.PropTypes.object.isRequired
   },
-  getInitialState: function() {
-    console.log('getInitialState')
-    return {isLoading: true, playersInfo: []}
+  getInitialState: function () {
+    return {
+      isLoading: true,
+      playersInfo: [],
+    }
   },
-  componentWillMount: function() {
-    console.log('componentWillMount');
-  },
-  componentDidMount: function() {
+  componentDidMount: function () {
     var query = this.props.location.query;
-    // https://egghead.io/playlists/the-this-keyword-250c37d9
-    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo]).then(function(players) {
-      this.setState({
-        isLoading: false,
-        playersInfo: [players[0], players[1]]
-      })
-    }.bind(this))
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function (players) {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))
   },
   handleInitiateBattle: function () {
     this.context.router.push({
       pathname: '/results',
       state: {
-        playerInfo: this.state.playersInfo
+        playersInfo: this.state.playersInfo
       }
     })
   },
-  componentWillReceiveProps: function() {
-    console.log('componentWillReceiveProps')
-  },
-  componentWillUnmount: function() {
-    console.log('componentWillUnmount')
-  },
-  render: function() {
+  render: function () {
     return (
-      <ConfirmBattle 
-      isLoading={this.state.isLoading} 
-      onInitiateBattle={this.handleInitiateBattle}
-      playersInfo={this.state.playersInfo} />)
+      <ConfirmBattle
+        isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
+        playersInfo={this.state.playersInfo} />
+    )
   }
 });
 
--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -1,26 +1,60 @@
 var axios = require('axios');
+
 var id = "YOUR_CLIENT_ID";
 var sec = "YOUR_SECRET_ID";
 var param = "?client_id=" + id + "&client_secret=" + sec;
 
-function getUserInfo(username) {
+function getUserInfo (username) {
   return axios.get('https://api.github.com/users/' + username + param);
 }
-var helpers = {
-  getPlayersInfo: function(players) {
-    // fetch some data from git hub (utilize axios)
-    return axios.all(players.map(function(username) {
 
-      return getUserInfo(username);
-    })).then(function(info) {
-      return info.map(function(user) {
-        console.log(user.data);
-        return user.data;
-      })
-    }).catch(function (err) {
-        console.warn('error in getplayers info '  + err);
-    })
-
-  }
+function getRepos (username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
 }
+
+function getTotalStars (repos) {
+  return repos.data.reduce(function (prev, current) {
+    return prev + current.stargazers_count
+  }, 0)
+}
+
+function getPlayersData (player) {
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function (totalStars) {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+function calculateScores (players) {
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ]
+}
+
+var helpers = {
+  getPlayersInfo: function (players) {
+    return axios.all(players.map(function (username) {
+      return getUserInfo(username)
+    }))
+    .then(function (info) {
+      return info.map(function (user) {
+        return user.data
+      })
+    })
+    .catch(function (err) {console.warn('Error in getPlayersInfo: ', err)})
+  },
+  battle: function (players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function (err) {console.warn('Error in getPlayersInfo: ', err)})
+  }
+};
+
 module.exports = helpers;
