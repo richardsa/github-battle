78402fa9512025e7940d7bafc69215ffabd69b3d
Richard Saldivar <rsaldiva@gmail.com>
2016-08-14 18:01:01 +0000

    continue at 3:00 Building a Highly Reusable React Componen video

---
+++ app/components/Loading.js
@@ -0,0 +1,46 @@
+var React = require('react');
+var styles= {
+    container: {
+        position: 'fixed',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        fontSize: '55px'
+    },
+    content: {
+        textAlign: 'center',
+        position: 'absolute',
+        width: '100%',
+        marginTop: '30px'
+    }
+    
+};
+
+var Loading = React.createClass({
+    getInitialState: function (){
+      return {
+          text: 'Loading'
+      }  
+    },
+    componentDidMount: function (){
+      var stopper = "Loading...";
+      this.interval = setInterval(function (){
+          if(this.state.text === stopper) {
+              this.setState({
+                  text: 'Loading'
+              })
+          }
+      }, 300)
+    },
+    render: function (){
+        return (
+                <div style={styles.container}>
+                    <p style={styles.content}>{this.state.text}</p>
+                </div>
+            )
+    }
+    
+});
+
+module.exports = Loading;
