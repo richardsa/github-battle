01a3ec0da34952ad0055abe681f9165d4f61e0be
Richard Saldivar <rsaldiva@gmail.com>
2016-08-14 17:47:53 +0000

    finish Private Stateless Functional React Components video

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,6 +4,8 @@ var styles = require('../styles');
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var Link = require('react-router').Link;
+var MainContainer = require('./MainContainer');
+
 
 function StartOver(){
     return (
@@ -16,18 +18,23 @@ function StartOver(){
 }
 
 function Results (props) {
+    if (props.isLoading === true) {
+        return (
+                <p>LOADING</p>
+            )
+    }
     if(props.scores[0] === props.scores[1]){
         return (
-            <div className="jumbotron col-sm-12 text-center" style={styles.transparentBG}>
+           <MainContainer>
                 <h1>It's a tie!</h1>
               <StartOver />
-            </div>
+            </MainContainer>
             )
     }
     var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
     var losingIndex = winningIndex === 0 ? 1 : 0;
     return (
-        <div className="jumbotron col-sm-12 text-center" style={styles.transparentBG}>
+        <MainContainer>
             <h1>Results</h1>
             <div className='col-sm-8 col-sm-offset-2'>
                 <UserDetailsWrapper header='Winner'>
@@ -39,7 +46,7 @@ function Results (props) {
                 </UserDetailsWrapper>
             </div>
             <StartOver />
-         </div>
+        </MainContainer>
         )
     
 }
