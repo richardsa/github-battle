ce503e1b7bd585e55e7025ff43d94693a74f2367
Richard Saldivar <rsaldiva@gmail.com>
2016-08-15 16:57:02 +0000

    add localtunnel

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes');
-
+console.log(window.thing.nope)
 ReactDOM.render(routes, document.getElementById('app'));
