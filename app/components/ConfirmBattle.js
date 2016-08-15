2b85cdb4c0fb0eaaaa7d58e8d87b93b938532d87
Richard Saldivar <rsaldiva@gmail.com>
2016-08-15 01:33:10 +0000

    finish Building a Highly Reusable React Component video

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -5,14 +5,11 @@ var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var MainContainer = require('./MainContainer');
-
-function puke(object){
-    return <pre>{JSON.stringify(object, null, ' ' )}</pre>
-}
+var Loading = require('./Loading');
 
 function ConfirmBattle (props) {
   return props.isLoading === true
-    ? <p>LOADING</p>
+    ? <Loading speed={800} text="Waiting"/>
     : <MainContainer>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
 var styles= {
     container: {
         position: 'fixed',
@@ -18,20 +19,38 @@ var styles= {
 };
 
 var Loading = React.createClass({
+    propTypes: {
+      text: PropTypes.string,
+      speed: PropTypes.number
+    },
+    getDefaultProps: function() {
+        return {
+            text: "Loading",
+            speed: 300
+        }
+    },
     getInitialState: function (){
+        this.originalText = this.props.text;
       return {
-          text: 'Loading'
+          text: this.originalText
       }  
     },
     componentDidMount: function (){
-      var stopper = "Loading...";
+      var stopper = this.originalText + "...";
       this.interval = setInterval(function (){
           if(this.state.text === stopper) {
               this.setState({
-                  text: 'Loading'
+                  text: this.originalText
+              })
+          } else {
+              this.setState({
+                  text: this.state.text + "."
               })
           }
-      }, 300)
+      }.bind(this), this.props.speed)
+    },
+    componentWillUnmount: function (){
+      clearInterval(this.interval);  
     },
     render: function (){
         return (
